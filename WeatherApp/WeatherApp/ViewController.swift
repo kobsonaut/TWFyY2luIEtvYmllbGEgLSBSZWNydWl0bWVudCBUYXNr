cfddeea88f3db8b6bd0b83fc0d8f83ad9fd8d389
Marcin Kobiela <>
2020-05-21 13:11:22 +0200

    Create empty project

---
+++ WeatherApp/WeatherApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WeatherApp
+//
+//  Created by Kobsonauta on 21/05/2020.
+//  Copyright © 2020 Kobsonauta. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
