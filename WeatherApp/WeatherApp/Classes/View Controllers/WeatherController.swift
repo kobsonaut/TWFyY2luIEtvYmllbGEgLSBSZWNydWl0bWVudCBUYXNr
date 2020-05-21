37363ab88c6914696c49637e1cce3c1f3d012476
Marcin Kobiela <>
2020-05-21 21:36:33 +0200

    Implement basic table view

--- WeatherApp/WeatherApp/Classes/View Controllers/WeatherController.swift
+++ WeatherApp/WeatherApp/Classes/View Controllers/WeatherController.swift
@@ -6,8 +6,48 @@
 //  Copyright © 2020 Kobsonauta. All rights reserved.
 //
 
-import Foundation
+import UIKit
 
-final class WeatherController: ViewController {
-    
+final class WeatherController: ViewController, UITableViewDataSource, UITableViewDelegate {
+
+    // MARK: Constants
+    struct Constants {
+        static let cell_id = "WeatherCell"
+    }
+
+    // MARK: Members
+    var tableView: UITableView = UITableView()
+    var items: [String] = ["Szczecin", "Warsaw", "London", "Vienna"]
+
+    override func loadView() {
+        super.loadView()
+
+        // Table view
+        tableView.dataSource = self
+        tableView.delegate = self
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: Constants.cell_id)
+        addSubviewToControllerContent(sub: tableView)
+    }
+
+    // MARK: Table view
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return items.count
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: Constants.cell_id, for: indexPath)
+        cell.textLabel?.text = self.items[indexPath.row]
+        return cell
+    }
+
+    // MARK: Layout
+    override func layoutElements() {
+        super.layoutElements()
+
+        var cframe = CGRect.zero
+
+        let sub = self.tableView
+        cframe = self.controllerContentView.bounds
+        sub.frame = cframe
+    }
 }
